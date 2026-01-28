37c64425e369db077959ddc0102dce4c3d20f812
Yashwanth G M <yashwanthgm692@gmail.com>
2026-01-28 13:08:03 +0530

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -1,4 +1,33 @@
+//import java.util.Scanner;
 
-public class Day1 {
+//Q1 given 3 input values write program to get the sum of pairs???  
+//public class Day1 {
+//	public static void main(String[]args) {
+//		Scanner scan = new Scanner(System.in);
+//		
+//		int input1 = scan.nextInt();
+//		int input2 = scan.nextInt();
+//		int input3 = scan.nextInt();
+//		
+//		System.out.println((input1+input2)+" "+(input1+input3)+" "+(input2+input3));
+//			
+//		scan.close();
+//	}
+//
+//}
 
-}
+// Q2 write a program to convert given input  Celsius to Fahrenheit ???
+
+//public class Day1{
+//	public static void main(String[]args) {
+//		Scanner scan=new Scanner(System.in);
+//		
+//		int c = scan.nextInt();
+//		
+//		float F = (c * 9.0f/5) + 32;
+//		System.out.println(F);
+//		
+//	}
+//}
+
+
