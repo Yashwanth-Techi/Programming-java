ff5bbe4f9bacca4bba7e2492bd0d5b90ec1a2e59
Yashwanth G M <yashwanthgm692@gmail.com>
2026-01-29 15:06:58 +0530

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -1,6 +1,8 @@
-//import java.util.Scanner;
+import java.util.Scanner;
 
 //Q1 given 3 input values write program to get the sum of pairs???  
+
+//import java.util.Scanner;
 //public class Day1 {
 //	public static void main(String[]args) {
 //		Scanner scan = new Scanner(System.in);
@@ -18,6 +20,7 @@
 
 // Q2 write a program to convert given input  Celsius to Fahrenheit ???
 
+//import java.util.Scanner;
 //public class Day1{
 //	public static void main(String[]args) {
 //		Scanner scan=new Scanner(System.in);
@@ -31,3 +34,103 @@
 //}
 
 
+// Q3 write a program to check the given number is even or odd???
+
+//import java.util.Scanner;
+////
+//class Day1{
+//	public static void main(String[] args) {
+//		Scanner scan = new Scanner(System.in);
+//		
+//		int input1 = scan.nextInt();
+//		
+//		if (input1 % 3 == 0) {
+//			System.out.println("The number is even");
+//		}
+//		else {
+//			System.out.println("The number  is odd");
+//		}
+//		scan.close();
+//	}
+//}
+
+
+// check the give is multiple of 10 ??
+
+//import java.util.Scanner;
+//
+//class Day1{
+//	public static void main(String[]args) {
+//		
+//		Scanner scan = new Scanner(System.in);
+//		
+//		int input1 = scan.nextInt();
+//		
+//		if (input1 % 2 == 0 || input1 % 10 == 0) {
+//			System.out.println("yes");
+//		}
+//		else
+//		{
+//			System.out.println("No");
+//		}
+//		scan.close();
+//	}
+//}
+
+//class Day1{
+//	public static void main(String[]args) {
+//		Scanner scan = new Scanner(System.in);
+//		
+//		int input1 = scan.nextInt();
+//		
+//		if (input1 % 2 == 0 && input1 % 3 == 0 && input1 % 7 == 0) {
+//			System.out.println("yes");
+//		}
+//		else
+//		{
+//			System.out.println("no");
+//		}
+//		
+//	}
+//}
+
+// Q7 check the given number is ending with '0' ??
+
+//class Day1{
+//	public static void main(String[]args) {
+//		Scanner scan = new Scanner(System.in);
+//		
+//		int input1  = scan.nextInt();
+//		
+//		if (input1 % 10 == 0) {
+//			System.out.println("yes it is ending with 0");
+//		}
+//		else
+//		{
+//			System.out.println("No it not ending with 0");
+//		}
+//		scan.close();
+//		
+//	}
+//}
+
+
+// Q8 check the number is ending  with 7 
+
+
+//class Day1{
+//	public static void main(String[]args) {
+//		Scanner scan = new Scanner(System.in);
+//		
+//		int input = scan.nextInt();
+//		
+//		if (i % 10 == 7 ) {
+//			System.out.println("yes number is ending with 7");
+//		}
+//		else
+//		{
+//			System.out.println("!No number is ending with 7");
+//		}
+//		scan.close();
+//	}
+//}
