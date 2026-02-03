1445a11863b8259f28ee7b53c6d3a3213c82ba2f
Yashwanth G M <yashwanthgm692@gmail.com>
2026-02-03 21:16:30 +0530

    Update Day1.java

--- Day1.java
+++ Day1.java
@@ -135,3 +135,163 @@ import java.util.Scanner;
 //	}
 //}
 
+//Q9  write a program to remove last digit of the  number N??
+
+//class Day1{
+//	public static void main(String[]args) {
+//		Scanner scan = new Scanner(System.in);
+//		
+//		int ip = scan.nextInt();
+//		
+//		System.out.println(ip/10);
+//      System.out.println(ip/100);  #to remove last 2 digits
+//      System.out.println(ip/1000); # to remove last 3 digits
+//		
+//	}
+//}
+
+
+//Q10 write a program to check whether give number is a two digits number are not ??
+
+//class Day1{
+//	public static void main(String[] args) {
+//		Scanner scan = new Scanner(System.in);
+//		
+//		int input1 = scan.nextInt();
+//		
+//		if (input1 > 9 && input1 < 100) {
+//			System.out.println("yes it have 2 digits ");
+//		}
+//		else {
+//			System.out.println("no it don't have 2 digits ");
+//			
+//		}
+//	}
+//}
+
+//Q11  check the given number is unicode value of uppercase character or not ??
+
+//class Day1{
+//	public static void main(String[] args) {
+//		Scanner scan = new Scanner(System.in);
+//
+//		int input1 = scan.nextInt();
+//
+//		if (input1 >= 65 && input1 <=90) {
+//			System.out.println("It is belong to Upper case");
+//		}
+//		else
+//		{
+//			System.out.println("It is not belonging to Upper case");
+//		}
+//
+//	}
+//}
+
+// check is it is lowerCase are not??
+
+//class Day1{
+//	public static void main(String[] args) {
+//		Scanner scan = new Scanner(System.in);
+//
+//		int input1 = scan.nextInt();
+//
+//		if(input1 >= 97 && input1 <= 122) {
+//
+//			System.out.println("It is belonging to lower case");
+//		}
+//		else 
+//		{
+//			System.out.println("It is not Lower Case");
+//		}
+//
+//	}
+//}
+
+
+//Q12 print equivalent character for the give unicode value
+
+//class Day1{
+//public static void main(String[] args) {
+//	Scanner scan = new Scanner(System.in);
+//	
+//	int input1 = scan.nextInt();
+//	
+//	System.out.println((char)input1);
+//	
+//	}
+//}
+
+//Q13 given an intiger representing a Unicode Value write a program to determine whether it 
+//coresponds to an upperCase Letter or LowerCase Letter or Numeric Digit or None of the above(N/A) ???
+
+//class Day1{
+//public static void main(String[] args) {
+//	Scanner scan = new Scanner(System.in);
+//	
+//	int input1 = scan.nextInt();
+//	
+//	if (input1 >=65 && input1 <=90) {
+//		System.out.println("it is Uppercase letter");
+//	}
+//	else if(input1 >=97 && input1 <= 122 ) {
+//		System.out.println("It is LowerCase letter");
+//	}
+//	else if(input1 >=48 && input1 <= 57) {
+//		System.out.println("it is numeric value");
+//	}
+//	else 
+//	{
+//		System.out.println("!Invalid Input");
+//	}
+//	
+//	}
+//}
+
+
+// write a program to print the grade for the give marks refer the grading System ??
+
+// example :- input(91-100 = A+),(81-90= A),(71-80 = B+),(61-70 = B),(51 - 60 = C+) (35-50 = C),(0-35 = fail!)
+
+//class Day1{
+//	public static void main(String[] args) {
+//		Scanner scan = new Scanner(System.in);
+//
+//		int input1 = scan.nextInt();
+//
+//		if(input1 >= 91 && input1 <=100) {
+//			System.out.println("A+");
+//		}
+//		else if(input1 >= 81 && input1 <= 90){
+//			System.out.println("A");
+//		}
+//		else if(input1 >= 71 && input1 <=80) {
+//			System.out.println("B+");
+//		}
+//		else if(input1 >= 61 && input1 <= 70) {
+//			System.out.println("B");
+//		}
+//		else if(input1 >= 51 && input1 <= 60) {
+//			System.out.println("C+");
+//		}
+//		else if (input1 >= 35 && input1 <= 50) {
+//			System.out.println("C");
+//		}
+//		else
+//		{
+//			System.out.println("fail!");
+//		}
+//
+//	}
+//}
+
+//class Day1{
+//public static void main(String[] args) {
+//	Scanner scan = new Scanner(System.in);
+//	
+//	int input1 = scan.nextInt();
+//	
+//	}
+//}
+
+
