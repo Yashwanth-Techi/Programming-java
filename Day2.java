60230b333697c51fc17d4819dae698ffe4941c72
Yashwanth G M <yashwanthgm692@gmail.com>
2026-02-07 19:09:33 +0530

    Add initial structure and comments for even number program
    Added comments and initial structure for a program to print even numbers up to a given integer.

--- Day2.java
+++ Day2.java
@@ -1,5 +1,21 @@
                                                                                                 //DAY2 Loop's programming
-public class Day2 {
 
-}
+//Q1) given an integer value n .write a program to print all the even numbers till n ??
+// ipmort java.util.Scanner;
+// public class Day2 {
+//   public static void main(String[]args){
+//     Scanner scan = new Scanner(System.in);
+    
+//     int = scan.nextInt();
+    
+//     for (int i=1;i<=10;i++){
+//       if(i%2==0){
+//         System.out.println(i);
+//       }
+//     }
+//    }
+// }
+
+
+
 
