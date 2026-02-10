5e45e89be7cc841428dfedc4dceaf17fc5e489f4
Yashwanth G M <yashwanthgm692@gmail.com>
2026-02-10 12:27:40 +0530

    Day2.java with updated even/odd logic

--- Day2.java
+++ Day2.java
@@ -1,21 +1,106 @@
-                                                                                                //DAY2 Loop's programming
-
-//Q1) given an integer value n .write a program to print all the even numbers till n ??
-// ipmort java.util.Scanner;
-// public class Day2 {
-//   public static void main(String[]args){
-//     Scanner scan = new Scanner(System.in);
-    
-//     int = scan.nextInt();
-    
-//     for (int i=1;i<=10;i++){
-//       if(i%2==0){
-//         System.out.println(i);
-//       }
-//     }
-//    }
-// }
+                                                                     // Loop's 
+import java.util.Scanner;
 
 
 
+//Q1 given an interger value write a program to print all the even number of 'n'??
+//public class Day2 {
+//	public static void main(String[]args) {
+//		Scanner scan = new Scanner(System.in);
+//		
+//		int n = scan.nextInt();
+//		
+//		for(int i=1;i<=n;i++) {
+//			if (i%2==0) {
+//				System.out.println(i);
+//			}
+//		}
+//		
+//		
+//	}
+//
+//}
+
+// annother opproch
+
+//public class Day2 {
+//	public static void main(String[]args) {
+//		Scanner scan = new Scanner(System.in);
+//		
+//		int n = scan.nextInt();
+//		
+//		for(int i=2;i<=n;i=i+2) {
+//			System.out.println(i);
+//		}
+//	}
+//}
+
+
+//Q2 given an integer value n write a program to print all the odd numbers till 'n'??
+
+//public class Day2 {
+//	public static void main(String[]args) {
+//		Scanner scan = new Scanner(System.in);
+//		
+//		int input1 = scan.nextInt();
+//		
+//		for(int i=1;i<=input1;i++) {
+//			if(i%2!=0) {
+//				System.out.println(i);
+//			}
+//		}
+//	}
+//}
+
+// opproch 2
+
+//public class Day2 {
+//	public static void main(String[]args) {
+//		Scanner scan = new Scanner(System.in);
+//		
+//		int input1 = scan.nextInt();
+//		
+//		for(int i=1;i<=input1;i=i+2) {
+//			System.out.println(i);
+//		}
+//	}
+//}
+
+
+//Q3 write program to print all add numbers in revers order??
+
+
+//public class Day2 {
+//	public static void main(String[]args) {
+//		Scanner scan = new Scanner(System.in);
+//		
+//		int input1 = scan.nextInt();
+//		
+//		if (input1%2==0) {          
+//			input1 = input1-1;
+//		}
+//		
+//		for(int i=input1;i>=1;i=i-2) {
+//			System.out.println(i);
+//			
+//		}
+//	}
+//}
+
+//opproch 2
+
+//public class Day2 {
+//	public static void main(String[]args) {
+//		Scanner scan = new Scanner(System.in);
+//		
+//		int input1 = scan.nextInt();
+//		for(int i=((input1%2==0)?input1-1:input1);i>=1;i=i+2){
+//			System.out.println(i);
+//		}
+//	}
+//
+//}
+
+
+//Q4 Given three integer value 'n' a and B. write a program to print all the multiples of a and b till "n"??
 
