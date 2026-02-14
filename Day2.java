c4d24ed782bd96de5203fde171009ba44954f17d
Yashwanth G M <yashwanthgm692@gmail.com>
2026-02-14 23:36:57 +0530

    code and added new questions

--- Day2.java
+++ Day2.java
@@ -121,4 +121,30 @@ import java.util.Scanner;
 // }
 
 
+//Q5) given an integer n write a program to print all the factors of n?/
+
+// class Day2{
+// 	public static void allFactors(int n) {
+		
+// 		for (int i=1;i<=n;i++) {
+// 			if(n%i==0) {
+// 				System.out.println(i);
+// 			}
+// 		}
+		
+// 	}
+	
+// 	public static void main(String[]args) {
+// 		Scanner sc = new Scanner(System.in);
+		
+// 		System.out.println("Enter integer n: ");
+// 		int n = sc.nextInt();
+		
+// 		allFactors(n);
+// 	}
+	
+// }
+
+
+
 
