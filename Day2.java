c94c5f0247cd2538d9e86887328d4321c58963b9
Yashwanth G M <yashwanthgm692@gmail.com>
2026-02-14 23:55:46 +0530

    Refactor Day2.java with multiple approaches

--- Day2.java
+++ Day2.java
@@ -170,6 +170,28 @@ import java.util.Scanner;
 // 	}
 // }
 
+//Q6)given an integer value n write a program to check whether n is prime or not!...
 
+//class Day2{
 
-
+//	public static String prime(int n) {
+//		int count = 0;
+//		for (int i=1;i<=n;i++) {
+//			if(n%i==0) {
+//				return "Not prime";
+//			}
+//		}
+//		return "It is Prime";	
+//	}
+//	
+//	public static void main(String[]args) {
+//		Scanner sc = new Scanner(System.in);
+//		
+//		System.out.println("Enter integer n: ");
+//		int n = sc.nextInt();
+//		
+//		System.out.println(prime(n));
+//		
+//	}
+//	
+//}
